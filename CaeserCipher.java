8d203e727e37c17182bfaca9498894cbae0d518b
rasagya2802 <gaderasagya1@gmail.com>
2025-04-09 18:11:46 +0530

    added psvm in caeser cipher

--- CaeserCipher.java
+++ CaeserCipher.java
@@ -8,11 +8,29 @@ public class CaeserCipher {
             if(Character.isLowerCase(ch)){
                 char c = (char) (((ch - 'a' + shift)% 26)+'a');
                 result.append(c);
-            } else {
+            } else if(Character.isUpperCase(ch)){ 
                 char c = (char) (((ch - 'A' + shift)%26)+'A');
                 result.append(c);
+            } else{
+                result.append(ch); // Non-alphabetic characters remain unchanged}
             }
         }
         return result.toString();
+    }
+     // Decrypt function
+     public static String decrypt(String text, int shift) {
+        return encrypt(text, 26 - shift); // Reverse shift
+    }
+
+    public static void main(String[] args) {
+
+        String plainText = "Hello World";
+        int key = 3;  // Shift of 3 positions
+
+        System.out.println("Plain Text: " + plainText);
+        String encrypted = encrypt(plainText, key);
+        System.out.println("Encrypted Text: " + encrypted);
+        String decrypted = decrypt(encrypted, key);
+        System.out.println("Decrypted Text: " + decrypted);
     }
 }
